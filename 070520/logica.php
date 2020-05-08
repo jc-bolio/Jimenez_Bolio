4d0eea4876c36f960f005d8e63d1f304e4d443be
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2020-05-07 21:30:55 -0500

    Update logica.php

--- 070520/logica.php
+++ 070520/logica.php
@@ -7,7 +7,7 @@ $res = $num1 + $num2 + $num3;
 echo"<table align='center' style='background-color: red; color: balck; border: 5px solid black;'>
 <tr>
     <td>
-        Resultado es : ".$res."
+        Resultado: ".$res."
     </td>
 </tr>
 </table>";
